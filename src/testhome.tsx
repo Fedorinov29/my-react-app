b8f6df56dfcf6b22752c34de1a5df9f3cbde4fae
Oleksii <a.fedorinov1993@gmail.com>
2021-02-28 20:31:05 +0200

    init commit

---
+++ src/testhome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './testhome.css'
+
+
+function Testhome() {
+   return (
+      <span className="Test-style">Some text</span>
+   );
+}
+
+export default Testhome;
