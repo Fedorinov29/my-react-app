4ab19617b3f3a47cb6030b05b45e279c25981343
Oleksii <a.fedorinov1993@gmail.com>
2021-03-08 19:57:59 +0200

    add picArray for cards

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -5,6 +5,7 @@ import Cards from '../pictures/cards'
 
 
 function Body() {
+   const picArray = ['../../assets/images/1.jpg', '../../assets/images/2.jpg', '../../assets/images/3.jpg', '../../assets/images/4.jpg'];
    return (
       <>
          <Container>
